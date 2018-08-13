81cf4f347c3b1fd44e168e3c475c406e1c40d8de
MJAlexander <monicaalexander@berkeley.edu>
2018-08-13 09:20:19 -0400

    some thoughts about cohort v stationary

---
+++ code/alternative_daughters.R
@@ -0,0 +1,37 @@
+
+surviving_daughters_cohort <- function(df, # dataframe with LX and Fx
+                                       age_a, # age of mother
+                                       ffab = 0.4886 # fraction female at birth)
+){
+  x_vec <- seq(15,age_a, by = 5)
+  daughter_cohorts <- year - (age_a - x_vec)
+  
+  # calculate based on cohorts
+  LF_prod <- c()
+  for(i in 1:length(x_vec)){
+    this_L <- df %>% filter(age == age_a - x_vec[i], cohort == daughter_cohorts[i]) %>% select(Lx_cohort) %>% pull()
+    this_F <- df %>% filter(age == x_vec[i], cohort == mother_cohort) %>% select(Fx) %>% pull()
+    LF_prod <- c(LF_prod, this_L/10^5*this_F/10^3*ffab)
+  }
+  
+  sum(LF_prod)
+}
+
+surviving_daughters_mean_age <- function(df, # dataframe with LX and Fx
+                                         age_a, # age of mother
+                                         ffab = 0.4886 # fraction female at birth)
+  
+){
+  mother_cohort <- year - age_a
+  x_vec <- seq(15,age_a, by = 5)
+  
+  # calculate based on period of mean childbearing age = 30
+  LF_prod <- c()
+  for(i in 1:length(x_vec)){
+    this_L <- df %>% filter(age == age_a - x_vec[i], year == mother_cohort+30) %>% select(Lx) %>% pull()
+    this_F <- df %>% filter(age == x_vec[i], year == mother_cohort+30) %>% select(Fx) %>% pull()
+    LF_prod <- c(LF_prod, this_L/10^5*this_F/10^3*ffab)
+  }
+  
+  sum(LF_prod)
+}
